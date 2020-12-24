e0184c4722f681ef50d21df3a5b0a3a6eaf41674
wasimalam072 <45271777+wasimalam072@users.noreply.github.com>
2020-12-24 12:09:22 +0530

    Add files via upload

---
+++ ZeaEye.iOS/CustomControlsIOS/CustomEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using ZeaEye.CustomControls;
+using ZeaEye.iOS.CustomControlsIOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace ZeaEye.iOS.CustomControlsIOS
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (Control == null || this.Element == null)
+                return;
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
---
+++ ZeaEye.iOS/Services/AuthIOS.cs
@@ -0,0 +1,70 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+using ZeaEye.iOS.Services;
+using ZeaEye.Services;
+using Xamarin.Forms;
+using Firebase.Auth;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AuthIOS))]
+namespace ZeaEye.iOS.Services
+{
+    public class AuthIOS : IAuth
+    {
+        public string GetUserId()
+        {
+            var user = Auth.DefaultInstance.CurrentUser;
+            return user.Uid;
+        }
+
+        public bool IsSignIn()
+        {
+            var user = Auth.DefaultInstance.CurrentUser;
+            return user != null;
+        }
+
+        public async Task<string> LogInWithEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                var user = await Auth.DefaultInstance.SignInWithPasswordAsync(email, password);
+                return await user.User.GetIdTokenAsync();
+            }
+            catch (Exception e)
+            {
+                return string.Empty;
+            }
+        }
+
+        public bool SignOut()
+        {
+            try
+            {
+                _ = Auth.DefaultInstance.SignOut(out NSError error);
+                return error == null;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public async Task<string> SignUpWithEmailAndPassword(string email, string password)
+        {
+            try
+            {
+                var newUser = await Auth.DefaultInstance.CreateUserAsync(email, password);
+                return await newUser.User.GetIdTokenAsync();
+            }
+            catch(Exception e)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
